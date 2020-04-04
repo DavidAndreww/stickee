0484ba2f94c8a5018bed345d5c1176b1a5cded87
David Bille <d_bille@yahoo.com>
2020-04-04 10:52:31 -0500

    adding comments to notes.js controller

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,10 +1,13 @@
-const express = require("express");
+
 
 const toDo = [{msg: 'hi', id: 1}, {msg: 'bye', id: 2}]
 const toPlan = [2]
 const toDelegate = [3]
 const toDelete = [4]
 
+
+// @desc    gets all stickee note entries
+// @route   GET /sticky
 const getNotes = (req, res, next) => {
   res.json({toDo, toPlan, toDelegate, toDelete})
 };
