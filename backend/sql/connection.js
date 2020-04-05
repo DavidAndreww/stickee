1d3cc833a34323e9b585ffbdad5181ed7dd01e1c
David Bille <d_bille@yahoo.com>
2020-04-05 14:38:49 -0500

    created SQL DB. connection instance is set up and ready to store data

--- backend/sql/connection.js
+++ backend/sql/connection.js
@@ -6,9 +6,9 @@ class Connection {
       console.log('creating mysql connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: '35.238.27.129',
+        host: '34.70.199.15',
         user: 'root',
-        password: 'first-instance',
+        password: 'st!ck33',
         database: 'admin',
         port: 3306
       })
