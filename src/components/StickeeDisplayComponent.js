0ccc16f01fd4cfc8a919593b2902e7116d0b2f8e
David Bille <d_bille@yahoo.com>
2020-04-06 16:39:14 -0500

    refactoring

--- src/components/StickeeDisplayComponent.js
+++ src/components/StickeeDisplayComponent.js
@@ -1,36 +1,6 @@
 import React from "react";
 import StickeeNote from "./StickeeNote";
 
-// const StickeeDisplayComponent = ({ state, removeSticky }) => {
-//   const StickeeDisplay = ({ array, group }) => {
-//     return (
-//       <div className="sticky-box">
-//         {array.length > 0 && <h5>Do</h5>}
-//         <div className={group}>
-//           {array.map(note => (
-//             <StickeeNote
-//               key={note.id}
-//               id={note.id}
-//               group={group}
-//               message={note.message}
-//               removeSticky={removeSticky}
-//             />
-//           ))}
-//         </div>
-//       </div>
-//     );
-//   };
-
-//   return (
-//     <div className="priority-field">
-//       <StickeeDisplay array={state.do} group="do" />
-//       <StickeeDisplay array={state.plan} group="plan" />
-//       <StickeeDisplay array={state.delegate} group="delegate" />
-//       <StickeeDisplay array={state.delete} group="delete" />
-//     </div>
-//   );
-// };
-
 const StickeeDisplayComponent = ({ notes, removeSticky }) => {
   const StickeeDisplay = ({ notes, priority }) => {
     const array = notes.filter((note) => note.type === priority);
