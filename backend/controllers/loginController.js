c1cf2c88d000115c5de1f21ae16acc7718617ced
David Bille <d_bille@yahoo.com>
2020-04-08 15:11:38 -0500

    trying to set up backend

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,4 +1,8 @@
 const { users } = require("../data");
+const mysql = require('mysql')
+const pool = require('../sql/connection')
+const { handleSQLError } = require('../sql/error')
+
 // @desc    verifies login credentials
 // @route   GET /
 const userLogin = (req, res, next) => {
@@ -19,10 +23,22 @@ const userLogin = (req, res, next) => {
 // @desc    adds new user info to db
 // @route   POST /
 const newUserSignup = (req, res, next) => {
-  const userData = { ...req.body };
-  const id = users[users.length - 1].id;
-  const newUser = { ...userData, _id: id + 1 };
-  res.send(newUser);
+  let { email, password } = req.body
+  email = email.split('.')[0]
+  email = email.split('@').join('')
+  console.log(email)
+  let sql = "INSERT INTO ?? (??, ??) VALUES (??, ??)"; 
+  const userInput = ['users', 'email', '_password', `'${email}'`, `'${password}'`]
+
+  sql = mysql.format(sql, userInput);
+  console.log(sql)
+
+  // pool.query(sql, (err, results)=>{
+  //   if (err) return handleSQLError(res, err)
+  //   return res.status(201).json({new_user: results})
+  // })
+  
+
 };
 
 module.exports = { userLogin, newUserSignup };
