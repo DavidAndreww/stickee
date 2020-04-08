7d1248b613fe28765fd7e3590c50eafcf1f53451
David Bille <d_bille@yahoo.com>
2020-04-07 20:29:02 -0500

    working on functionality to find user by email for login

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,20 +1,24 @@
-const { users } = require('../data')
+const { users } = require("../data");
 // @desc    verifies login credentials
 // @route   GET /
 const userLogin = (req, res, next) => {
   const { email, pass } = req.body;
-  console.log('email: ', email)
-  console.log('pass: ', pass)
-  res.send(req.body)
-}
+  try {
+    let found = users.filter((user) => user.email === email) ? 'true': 'false';
+    console.log(found)
+
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
 // @desc    adds new user info to db
 // @route   POST /
 const newUserSignup = (req, res, next) => {
-  const userData = { ...req.body }
+  const userData = { ...req.body };
   const id = users[users.length - 1].id;
-  const newUser = { ...userData, _id: id + 1 }
-  res.send(newUser)
-}
+  const newUser = { ...userData, _id: id + 1 };
+  res.send(newUser);
+};
 
-module.exports = { userLogin, newUserSignup }
+module.exports = { userLogin, newUserSignup };
