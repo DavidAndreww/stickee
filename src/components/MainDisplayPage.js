7fa8c139c5a88d8879b0bf1daa8849b92d3ac386
David Bille <d_bille@yahoo.com>
2020-04-12 10:02:05 -0500

    added comment to address issue regarding state id updating dynamically

--- src/components/MainDisplayPage.js
+++ src/components/MainDisplayPage.js
@@ -10,6 +10,7 @@ class MainDisplayPage extends React.Component {
     message: "",
     important: true,
     urgent: true,
+    // note ID needs to update based on last note id in DB
     id: 1,
   };
 
@@ -28,8 +29,6 @@ class MainDisplayPage extends React.Component {
 
   removeSticky = (e) => {
     let id = parseInt(e.target.id);
-    // let newArray = this.state.notes.filter((object) => object.id !== id);
-    // this.setState({ notes: newArray });
     let [toBeDeleted] = this.state.notes.filter((object) => object.id === id);
     
     fetchData('/sticky', {
