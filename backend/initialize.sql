39c3842ffc8eb362a6bbb397371610fb28497c51
David Bille <d_bille@yahoo.com>
2020-04-12 13:51:00 -0500

    trying to fix initialize.sql file for proper table set up

--- backend/initialize.sql
+++ backend/initialize.sql
@@ -9,10 +9,12 @@ CREATE TABLE users (
 
 CREATE TABLE notes(
   id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
   note_id INT NOT NULL,
   note_message VARCHAR(255),
   note_type VARCHAR(255),
   PRIMARY KEY(id),
-  FOREIGN KEY (id)
+  FOREIGN KEY (user_id)
   REFERENCES users (id)
+    ON DELETE CASCADE
 );
