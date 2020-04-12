d03a9705a667e01f3284493972880c5d8afc0ad2
David Bille <d_bille@yahoo.com>
2020-04-12 14:46:44 -0500

    tables working in SQL and able to add new users, as well as get, create and delete notes

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -11,7 +11,11 @@ const getNotes = (req, res, next) => {
   let id = notes.length > 0 ? notes[notes.length -1].id : 1
 
   res.json({notes, id})
-  // 
+
+  // get all notes for user by their unique ID
+  // SELECT * 
+  // FROM notes
+  // WHERE notes.user_id = 1
 };
 
 // @desc    adds new stickee note
@@ -21,6 +25,10 @@ const addNotes = (req, res, next) => {
   res.json(newNote)
   // add new note to DB
   // return array of all notes in db including new note
+
+// user ID needs to be generated somehow....
+// INSERT INTO notes (user_id, note_id, note_message, note_type)
+// VALUES (1, 3, 'Make artwork', 'do')
 }
 
 // @desc    deletes stickee note
@@ -29,6 +37,13 @@ const deleteNotes = (req, res, next) => {
   let { note_id } = req.body;
   res.json({deletedNote: note_id})
   //return array of all notes in DB minus the deleted note
+
+
+//  DELETE FROM 
+// 	notes
+// WHERE
+// 	user_id = 1 && note_id = 2 
+
 }
 
 module.exports = { getNotes, addNotes, deleteNotes }
