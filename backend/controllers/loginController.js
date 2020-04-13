8bd9c69c7cf743d9e18985caa66efb20e3bd44f7
David Bille <d_bille@yahoo.com>
2020-04-12 23:24:24 -0500

    holy cow SQL syntax is fickle af. syntax to add new user through postman working!

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,6 +1,6 @@
 const { users } = require("../data");
 const mysql = require("mysql");
-const pool = require('../sql/connection')
+const pool = require("../sql/connection");
 const { handleSQLError } = require("../sql/error");
 
 // JOIN TABLE funcionality on login, to create new table that holds only those notes associated with that users user_id?
@@ -20,27 +20,21 @@ const userLogin = (req, res, next) => {
 // @route   POST /signup
 const newUserSignup = (req, res, next) => {
   let { email, password } = req.body;
-  email = email.split(".").join("");
-  res.json({
-    message: "New user created",
-    user: {
-      email: req.body.email,
-      password: password,
-    },
-  });
 
-  let sql = 'INSERT INTO users (email, _password) VALUES (??, ??)'
-  const replacements = [ `${email}`,'123']
-  sql = mysql.format(sql, replacements)
-  console.log(sql);
-
-  pool.query(sql, (err, results) => {
-    if (err) return handleSQLError(res, err);
-    return res.status(201).json({
-      message: "User Successfully Created",
-      new_user: results,
-    });
-  });
+  pool.query(
+    "INSERT INTO users (email, _password) VALUES ('" +
+      email +
+      "', '" +
+      password +
+      "')",
+    (err, results) => {
+      if (err) return handleSQLError(res, err);
+      return res.status(201).json({
+        message: "User Successfully Created",
+        new_user: { email: email, password: password, id: results.insertId },
+      });
+    }
+  );
 };
 
 module.exports = { userLogin, newUserSignup };
--- backend/sql/connection.js
+++ backend/sql/connection.js
@@ -1,16 +1,17 @@
 const mysql = require('mysql')
+const colors = require('colors')
 
 class Connection {
   constructor() {
     if (!this.pool) {
-      console.log('creating mysql connection...')
+      console.log('creating MySQL connection...'.blue.bold)
       this.pool = mysql.createPool({
         connectionLimit: 100,
         host: '34.70.199.15',
         user: 'root',
         password: 'st!ck33',
         database: 'admin',
-        port: 3306
+        // port: 3306
       })
 
       return this.pool
