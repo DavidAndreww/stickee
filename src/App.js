11ca189b89cfee2e92d600e23c71073af7e94a78
David Bille <d_bille@yahoo.com>
2020-04-13 09:33:13 -0500

    set up redux store to allow info to be passed into app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import Router from "./Router";
 import Header from "./components/Header";
+import store from "./redux/store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app">
-        <Header />
-        <Router />
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="app">
+          <Header />
+          <Router />
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
