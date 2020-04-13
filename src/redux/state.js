97b49aba45628e3d2f552a802b7a20a16b6ca43e
David Bille <d_bille@yahoo.com>
2020-04-13 09:17:37 -0500

    installed redux, react-redux dependencies. created base files and state

---
+++ src/redux/state.js
@@ -0,0 +1,14 @@
+const state = {
+  email: '',
+  password: '',
+  isNewUser: false,
+  user_id: null,
+  notes: [],
+  stickyFormOpen: false,
+  message: '',
+  important: true,
+  urgent: true,
+  note_id: 1,
+}
+
+export default state;
