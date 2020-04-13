25fc644dd3196825ddab6f17a9422c9d08140162
David Bille <d_bille@yahoo.com>
2020-04-13 09:41:04 -0500

    created reducer file base. will start working functionality and determine which pieces of state may be left out of redux

---
+++ src/redux/reducers.js
@@ -0,0 +1,44 @@
+import { combineReducers } from 'redux;'
+
+const email = (state = '', action) => {
+
+}
+
+const password = (state = '', action) => {
+
+}
+
+const isNewUser = (state = false, action) =>{
+
+}
+
+const user_id = (state = null, action) => {
+
+}
+
+const notes = (state = [], action) => {
+
+}
+
+const stickyFormOpen = (state = false, action) => {
+
+}
+
+const message = (state = '', action) => {
+
+}
+
+const important = (state = true, action) => {
+
+}
+
+const urgent = (state = true, action) => {
+
+}
+
+const note_id = (state = 1, action) => {
+  
+}
+
+
+export default combineReducers ({})
