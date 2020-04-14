9da4aec3d1261e9e9f8c57e3a644d791b82660bc
David Bille <d_bille@yahoo.com>
2020-04-13 19:52:07 -0500

    scrapped redux for now. focusing more on cleaning up react components and then will make decision on what data, if any, is appropriate for redux.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 import Router from "./Router";
 import Header from "./components/Header";
-import store from "./redux/store";
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <div className="app">
-          <Header />
-          <Router />
-        </div>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <Router />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/MainDisplayPage.js
+++ src/components/MainDisplayPage.js
@@ -11,8 +11,7 @@ class MainDisplayPage extends React.Component {
     important: true,
     urgent: true,
     // note ID needs to update based on last note id in DB
-    id: 1,
-    userId: null
+    note_id: 1,
   };
   
 
@@ -21,7 +20,7 @@ class MainDisplayPage extends React.Component {
     const json = await response.json()
     let data = [json][0].notes
     let id = json.id
-    this.setState({notes: data, id: id})
+    this.setState({notes: data, note_id: id})
   }
   
   componentDidMount(){
@@ -63,12 +62,11 @@ class MainDisplayPage extends React.Component {
     this.setState({ message: e.target.value });
   };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const notes = this.state.notes;
+  createNewStickee = () => {
+    const message = this.state.message
     const urgent = this.state.urgent;
     const important = this.state.important;
-    const id = this.state.id;
+    const id = this.state.note_id;
     let type;
 
     important && urgent && (type = 'do')
@@ -76,32 +74,32 @@ class MainDisplayPage extends React.Component {
     !important && urgent && (type = 'delegate')
     !important && !urgent && (type = 'delete')
 
-    const totalNotes = notes.filter((notes) => notes.type === type);
-
-    if (totalNotes.length >= 6) {
-      window.alert(`Finish some of your "${type}" tasks before adding more! `);
-      this.toggleAddStickyForm();
-      return false;
-    }
-
     const newNote = {
       note_id: id,
       note_type: type,
-      note_message: this.state.message,
-    };
-    this.setState({
-      notes: [...notes, newNote],
-      message: "",
-      id: id + 1,
-    });
+      note_message: message,
+    }
+    return (newNote)
+  }
 
-    fetchData('/sticky', {
-      method: 'POST',
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        newNote: newNote
-      })
-    })
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    
+    // this.setState({
+    //   notes: [...notes, newNote],
+    //   message: "",
+    //   id: id + 1,
+    // });
+    console.log(this.createNewStickee())
+
+    // fetchData('/sticky', {
+    //   method: 'POST',
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify({
+    //     newNote: this.createNewStickee()
+    //   })
+    // })
     this.toggleAddStickyForm();
   };
 
