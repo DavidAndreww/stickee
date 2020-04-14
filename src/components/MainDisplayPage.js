2598db4fd506f61b29dd9ed04308c3016eda2dcc
David Bille <d_bille@yahoo.com>
2020-04-13 20:04:00 -0500

    made function to create stickee note to cut down on responsibilities of handleSubmit function

--- src/components/MainDisplayPage.js
+++ src/components/MainDisplayPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import StickeeDisplayComponent from "./StickeeDisplayComponent";
 import AddStickyForm from "./AddStickyForm";
-import { fetchData } from '../helperFunctions'
+import { fetchData } from "../helperFunctions";
 
 class MainDisplayPage extends React.Component {
   state = {
@@ -13,34 +13,33 @@ class MainDisplayPage extends React.Component {
     // note ID needs to update based on last note id in DB
     note_id: 1,
   };
-  
 
-  fetchNotes = async function(path, payload){
-    const response = await fetch(path, payload)
-    const json = await response.json()
-    let data = [json][0].notes
-    let id = json.id
-    this.setState({notes: data, note_id: id})
-  }
-  
-  componentDidMount(){
-    this.fetchNotes('/sticky', {
-      method: 'GET',
-      headers: { "Content-Type": 'application/json' }
-    })
+  fetchNotes = async function (path, payload) {
+    const response = await fetch(path, payload);
+    const json = await response.json();
+    let data = [json][0].notes;
+    let id = json.id;
+    this.setState({ notes: data, note_id: id });
+  };
+
+  componentDidMount() {
+    this.fetchNotes("/sticky", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
   }
 
   removeSticky = (e) => {
     let id = parseInt(e.target.id);
     let [toBeDeleted] = this.state.notes.filter((object) => object.id === id);
-    
-    fetchData('/sticky', {
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' },
+
+    fetchData("/sticky", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        note_id: toBeDeleted.id
-      })
-    })
+        note_id: toBeDeleted.id,
+      }),
+    });
   };
 
   toggleAddStickyForm = () => {
@@ -62,44 +61,42 @@ class MainDisplayPage extends React.Component {
     this.setState({ message: e.target.value });
   };
 
-  createNewStickee = () => {
-    const message = this.state.message
+  createNewStickeeObject = () => {
+    const message = this.state.message;
     const urgent = this.state.urgent;
     const important = this.state.important;
     const id = this.state.note_id;
     let type;
 
-    important && urgent && (type = 'do')
-    important && !urgent && (type = 'plan')
-    !important && urgent && (type = 'delegate')
-    !important && !urgent && (type = 'delete')
+    important && urgent && (type = "do");
+    important && !urgent && (type = "plan");
+    !important && urgent && (type = "delegate");
+    !important && !urgent && (type = "delete");
 
     const newNote = {
       note_id: id,
       note_type: type,
       note_message: message,
-    }
-    return (newNote)
-  }
+    };
+    return newNote;
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
 
-    
+    fetchData("/sticky", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        newNote: this.createNewStickeeObject(),
+      }),
+    });
+
     // this.setState({
     //   notes: [...notes, newNote],
     //   message: "",
     //   id: id + 1,
     // });
-    console.log(this.createNewStickee())
-
-    // fetchData('/sticky', {
-    //   method: 'POST',
-    //   headers: { "Content-Type": "application/json" },
-    //   body: JSON.stringify({
-    //     newNote: this.createNewStickee()
-    //   })
-    // })
     this.toggleAddStickyForm();
   };
 
