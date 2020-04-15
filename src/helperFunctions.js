0f370da4042b2bc3ef7b2e3ec06c75b5a1c437b8
David Bille <d_bille@yahoo.com>
2020-04-14 19:12:03 -0500

    trying to make fetch requests reusable. need to take a break

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -1,9 +1,6 @@
 const fetchData = async function (path, payload){
   const response = await fetch(path, payload)
   const json = await response.json()
-  console.log(json)
-  return json
-  // return that cookie?
 }
 
 export { fetchData }
