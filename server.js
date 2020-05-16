5c87ce0b71586c69e0e5e249b584fc279787189e
David Bille <d_bille@yahoo.com>
2020-05-16 18:14:33 -0500

    breaking things

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.json());
 const notesRouter = require("./backend/routes/notesRouter");
 const loginRouter = require("./backend/routes/loginRouter");
 
-app.use("/api/login", loginRouter);
-app.use("/api/stickee", notesRouter);
+app.use("/", loginRouter);
+app.use("/stickee", notesRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("./build"));
