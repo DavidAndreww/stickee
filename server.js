af89274b658bcca9a1ad0005d2b0f6627b42b691
David Bille <d_bille@yahoo.com>
2020-05-16 17:01:45 -0500

    debugging

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-// const colors = require("colors");
+const path = require("path");
 
 dotenv.config({ path: "./backend/config/config.env" });
 
 app.use(express.json());
 
-const notesRouter = require('./backend/routes/notesRouter')
-const loginRouter = require('./backend/routes/loginRouter')
+const notesRouter = require("./backend/routes/notesRouter");
+const loginRouter = require("./backend/routes/loginRouter");
 
-app.use('/', loginRouter)
-app.use('/stickee', notesRouter)
+app.use("/", loginRouter);
+app.use("/stickee", notesRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./build"));
+}
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 8080;
 
