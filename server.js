b351ffa3ad774e3776f2be2df4818d72e300e76c
David Bille <d_bille@yahoo.com>
2020-05-17 01:49:36 -0500

    again

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use((req, res, next) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
 }
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 app.use("/api/", loginRouter);
 app.use("/stickee", notesRouter);
