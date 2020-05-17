fabcf305ca081ce8887e138fa5070abbd1dae5d0
David Bille <d_bille@yahoo.com>
2020-05-16 21:12:26 -0500

    ready to host?

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use("/", loginRouter);
 app.use("/stickee", notesRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./build"));
+  app.use(express.static("build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
