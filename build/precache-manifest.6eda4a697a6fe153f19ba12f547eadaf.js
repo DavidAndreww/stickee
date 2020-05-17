28306911b742e5c64c85d0df164dbfd0eba60515
David Bille <d_bille@yahoo.com>
2020-05-17 02:04:43 -0500

    please

---
+++ build/precache-manifest.6eda4a697a6fe153f19ba12f547eadaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e63402023c3d3c608b54a9133f8db2a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "462145b78eb4ed24e836",
+    "url": "./static/css/main.36422f02.chunk.css"
+  },
+  {
+    "revision": "70a602ab5550a5394feb",
+    "url": "./static/js/2.22fbbeb1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.22fbbeb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "462145b78eb4ed24e836",
+    "url": "./static/js/main.eb334c97.chunk.js"
+  },
+  {
+    "revision": "7acdc25947a25980c8c3",
+    "url": "./static/js/runtime-main.a4cb83df.js"
+  }
+]);
