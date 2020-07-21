b4e3de8c53194411c10d1a2da90362067c80753e
unknown <d_bille@yahoo.com>
2020-07-20 19:12:16 -0500

    installed packages. removed colors package use from middleware file.

--- backend/middleware/index.js
+++ backend/middleware/index.js
@@ -4,7 +4,7 @@ const colors = require('colors')
 const authenticate = (req, res, next) => {
   // token is equal to token value in request header key titled authorization
   const token = req.headers.authorization
-  console.log('new Token:',token.green.bold)
+  console.log('new Token:',token)
   try {
     let decoded = jwt.verify(token, "secret");
     console.log('Validated:', decoded)
