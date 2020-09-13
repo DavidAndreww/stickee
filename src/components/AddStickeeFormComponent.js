834817343c247717a64f5ca63ba8254ee56d11b3
unknown <d_bille@yahoo.com>
2020-09-13 14:23:19 -0500

    updated bcrypt and double to single quotes

--- src/components/AddStickeeFormComponent.js
+++ src/components/AddStickeeFormComponent.js
@@ -1,51 +1,64 @@
-import React from "react";
-import Switch from "@material-ui/core/Switch";
-import Dialog from "@material-ui/core/Dialog";
+import React from 'react'
+import Switch from '@material-ui/core/Switch'
+import Dialog from '@material-ui/core/Dialog'
 
 const AddStickeeFormComponent = (props) => {
   return (
     <div>
-      <button onClick={props.toggleAddStickeeForm} className="toggle-dialog-btn">
+      <button
+        onClick={props.toggleAddStickeeForm}
+        className='toggle-dialog-btn'
+      >
         +
       </button>
-      <Dialog open={props.stickeeFormOpen} className="add-stickee-dialog-component">
-        <form className="add-stickee-form-wrap" onSubmit={props.handleSubmit}>
-          <div className="form-left-side">
+      <Dialog
+        open={props.stickeeFormOpen}
+        className='add-stickee-dialog-component'
+      >
+        <form className='add-stickee-form-wrap' onSubmit={props.handleSubmit}>
+          <div className='form-left-side'>
             <label>I need to....</label>
-            <textarea rows="5" value={props.message} onChange={props.handleInputChange} />
+            <textarea
+              rows='5'
+              value={props.message}
+              onChange={props.handleInputChange}
+            />
           </div>
-          <div className="form-right-side">
-            <div className="switch">
+          <div className='form-right-side'>
+            <div className='switch'>
               <label>Is it important?</label>
               <br />
               no
               <Switch
-                id="important"
+                id='important'
                 checked={props.important}
                 onChange={props.handleImportantSwitch}
               />
               yes
             </div>
-            <div className="switch">
+            <div className='switch'>
               <label>Is it urgent?</label>
               <br />
               no
               <Switch
-                id="urgent"
+                id='urgent'
                 checked={props.urgent}
                 onChange={props.handleUrgentSwitch}
               />
               yes
             </div>
-            <button type="submit">Stick It!</button>
+            <button type='submit'>Stick It!</button>
           </div>
         </form>
-        <button className="stickee-form-back-btn" onClick={() => props.toggleAddStickeeForm(true)}>
+        <button
+          className='stickee-form-back-btn'
+          onClick={() => props.toggleAddStickeeForm(true)}
+        >
           Back
         </button>
       </Dialog>
     </div>
-  );
-};
+  )
+}
 
-export default AddStickeeFormComponent;
+export default AddStickeeFormComponent
